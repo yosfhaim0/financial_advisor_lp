1e7c664f4f2a2cb92960f57bd0b387f6e89e4c1b
YosefHaimAmrusi <yosfhaim0@gmail.com>
2025-09-02 21:37:14 +0700

    ref

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ import './index.css';
 function App() {
   return (
     <div className="App">
-      {/* Test if Tailwind is working */}
-      <div className="bg-red-500 text-white p-4 m-4 rounded">
-        TEST: If you see this with red background, Tailwind is working!
-      </div>
       <LandingPage config={landingPageConfig} />
     </div>
   );
